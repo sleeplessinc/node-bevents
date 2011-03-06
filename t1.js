d7a3e88a9b375224f21b28a9ac98086d1d6bdec9
Joe Hitchens <joe@sleepless.com>
2011-03-05 23:20:02 -0800

    -

---
+++ t1.js
@@ -0,0 +1,22 @@
+
+var o2j = function (o) { return JSON.stringify(o); };
+var j2o = function (j) { return JSON.parse(j); };
+var l = function (s) { console.log(s); };
+
+bevents = require("./bevents2");
+
+b1 = new bevents.BEvents(function() {
+
+	b1.on("foo", function (payload) {
+		l("__on__ foo "+o2j(payload));
+	});
+
+	setInterval(function() {
+		b1.emit("bar", {bar: 2});
+	}, 5000);
+
+});
+
+
+
+
---
+++ t2.js
@@ -0,0 +1,20 @@
+
+var o2j = function (o) { return JSON.stringify(o); };
+var j2o = function (j) { return JSON.parse(j); };
+var l = function (s) { console.log(s); };
+
+bevents = require("./bevents2");
+
+b2 = new bevents.BEvents(function() {
+
+	b2.on("bar", function (payload) {
+		l("__on__ bar "+o2j(payload));
+	});
+
+	setInterval(function() {
+		b2.emit("foo", {foo: 1});
+	}, 5000);
+
+});
+
+
